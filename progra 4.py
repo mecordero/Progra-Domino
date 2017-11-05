e00a91dc2dbe60d20987f0ac2275fdc427855fe8
Ma-lt <mlucio.alt@gmail.com>
2017-11-05 13:42:24 -0600

    Lee el archivo y resuelve los mapas

--- progra 4.py
+++ progra 4.py
@@ -150,34 +150,32 @@ for i in range (7):#pip1
 #crea la lista de mapas pips
 listaMapasPips = ListaMapasPips()
 
-#crea un mapa pips
+#lee el archivo e ingresa los mapas a la lista de Mapas Pips
+fileName = input("Ingrese el nombre del archivo: ")
+with open(str(fileName)) as f:
+    content = f.readlines()
+f.close()
+content = [x.strip() for x in content]
+content = [y.split(" ") for y in content]
+mapas = []
+mapa = []
+for i in range(1,len(content)+1):
+    temp=[]
+    for y in range(len(content[0])):
+        temp += [int(content[i-1][y])]
+    mapa.append(temp)
+    if i != 0 and i%7 == 0:
+        mapas.append(mapa)
+        mapa = []
 
+for i in range(len(mapas)):
+    matriz = mapas[i]
+    mapa_pips = MapaPips(matriz)
+    listaMapasPips.insertarMapa(mapa_pips)
 
-
-matriz = [ [5, 4, 3, 6, 5, 3, 4, 6],
-           [0, 6, 0, 1, 2, 3, 1, 1],
-           [3, 2, 6, 5, 0, 4, 2, 0],
-           [5, 3, 6, 2, 3, 2, 0, 6],
-           [4, 0, 4, 1, 0, 0, 4, 1],
-           [5, 2, 2, 4, 4, 1, 6, 5],
-           [5, 5, 3, 6, 1, 2, 3, 1] ]
-
-
-mapa_pips = MapaPips(matriz)
-
-listaMapasPips.insertarMapa(mapa_pips)
-
-matriz = [ [4, 2, 5, 2, 6, 3, 5, 4],
-           [5, 0, 4, 3, 1, 4, 1, 1],
-           [1, 2, 3, 0, 2, 2, 2, 2],
-           [1, 4, 0, 1, 3, 5, 6, 5],
-           [4, 0, 6, 0, 3, 6, 6, 5],
-           [4, 0, 1, 6, 4, 0, 3, 0],
-           [6, 5, 3, 6, 2, 1, 5, 3] ]
-
-mapa_pips = MapaPips(matriz)
-
-listaMapasPips.insertarMapa(mapa_pips)
-
+#resuelve la lista
 listaMapasPips.resolverLista()
+
+
+
 
